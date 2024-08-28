d12c05b60d046b5d5830c08b2214e87463ac3d49
More-Pe <peraltamorena.va@gmail.com>
2024-08-28 22:54:26 +0200

    refactor: add paths to navbar

--- src/components/Nabvar/Navbar.jsx
+++ src/components/Nabvar/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Surfer } from '../Surfer/Surfer';
+import { useAuth } from '../../contexts/AuthContext/AuthContext';
 
 export const Navbar = () => {
+	const navigate = useNavigate();
+	const { isLoggedIn, logout, isAdmin } = useAuth();
 	return (
 		<nav className='navbar'>
-			<ul className='navbar-items'>Navbar</ul>
+			{!isLoggedIn ? (
+				<>
+					<Surfer
+						className='nav-item'
+						path='/login'
+						content='Login'
+					/>
+					<Surfer
+						className='nav-item'
+						path='/register'
+						content='Register'
+					/>
+				</>
+			) : (
+				<>
+					<Surfer
+						className='nav-item'
+						path='/profile'
+						content='Profile'
+					/>
+					<Surfer
+						className='nav-item'
+						path='/home'
+						content='Home'
+					/>
+					<div
+						className='logout'
+						onClick={handleLogout}>
+						Logout
+					</div>
+				</>
+			)}
+			{isAdmin && (
+				<Surfer
+					className='nav-item'
+					path='/superadmin'
+					content='SuperAdmin'
+				/>
+			)}
 		</nav>
 	);
 };
--- src/components/Surfer/Surfer.jsx
+++ src/components/Surfer/Surfer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export const Surfer = ({ content, path }) => {
+export const Surfer = ({ content, path, className }) => {
 	const navigate = useNavigate();
 
-	return <div onClick={() => navigate(path)}>{content}</div>;
+	return (
+		<div
+			onClick={() => navigate(path)}
+			className={className}>
+			{content}
+		</div>
+	);
 };
